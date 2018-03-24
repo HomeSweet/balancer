a8f7c85c12c931393bd09cb9d239e697d1cac077
sergey <sergey@DESKTOP-DJUEO96>
2018-03-24 18:03:08 +0200

    Added validation rules

--- client-balancer/src/main/java/it/discovery/balancer/config/ServerConfiguration.java
+++ client-balancer/src/main/java/it/discovery/balancer/config/ServerConfiguration.java
@@ -2,11 +2,16 @@ package it.discovery.balancer.config;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @Validated
 public class ServerConfiguration {
 	
+	@Valid
 	private List<ServerDefinition> servers;
 }
--- client-balancer/src/main/java/it/discovery/balancer/config/ServerDefinition.java
+++ client-balancer/src/main/java/it/discovery/balancer/config/ServerDefinition.java
@@ -1,10 +1,16 @@
 package it.discovery.balancer.config;
 
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.validation.annotation.Validated;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@Validated 
 public class ServerDefinition {
+	@NotBlank
 	private String url;
 	
 	private boolean enabled;
